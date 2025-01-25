fa3bce0221a228bc5e3fcadb63d38da83ec54a5f
Anubhav-Vashishtha <anupamsk0007@gmail.com>
2025-01-26 05:13:54 +0530

    Done with search form

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-import express from 'express'
-import { makeAuthenticatedRequest } from './Helper Function/authen.js'
+import express from 'express';
+import { makeAuthenticatedRequest } from './Helper Function/authen.js';
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/send', async (req, res) => {
     try {
         const requestData = {
-            "Hotelcodes": 1000957, "Language": "en"
-            }; 
+            "Hotelcodes": 1000957,
+            "Language": "en"
+        };
 
         const data = await makeAuthenticatedRequest('/Hoteldetails', 'post', requestData);
-        res.json({ message: 'Success', data });
+        // res.json({ message: 'Success', data });
+        res.render('send', { title: 'Send Data', message: 'Submit your request' });
     } catch (error) {
         res.status(500).json({ error: 'Failed to send data' });
     }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
